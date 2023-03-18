d7cada1b9fc8a61ffd018ab2bccce493f555e61d
wodakh <115899643+wadokh@users.noreply.github.com>
2023-03-18 09:11:24 +0530

    make basic calculator

---
+++ script.js
@@ -0,0 +1,91 @@
+let firstNumber;
+let operatorSign='';
+let secondNumber;
+let i;
+
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    if (b===0){
+        alert("not defined")
+    }
+    else{
+    return a/b;
+    }
+}
+
+function operate(operator,a,b){
+    return operator(a,b)
+}
+
+let display=document.getElementById('display');
+const btns = document.querySelectorAll('.number')
+
+btns.forEach(btn => {
+
+   btn.addEventListener('click', event => {
+    if (i===1){
+        display.innerHTML=''
+        i=0;
+    }
+    display.innerHTML+= event.target.innerHTML ;
+   });
+
+});
+
+
+
+const mathOperators = document.querySelectorAll('.maths')
+
+mathOperators.forEach(mathOperator => {
+
+   mathOperator.addEventListener('click', event => {
+        let mathsymbol= event.target.innerHTML ;
+        if (operatorSign==='')
+        {firstNumber=Number(display.innerHTML);
+        i=1;
+        switch (mathsymbol){
+            case '+':
+                operatorSign=add;
+                break;
+            case '-':
+                operatorSign=subtract;
+                break;
+            case 'x':
+                operatorSign=multiply;
+                break;
+            case '÷':
+                operatorSign=divide;
+                break;
+                                    
+        }}
+        else{
+            secondNumber=Number(display.innerHTML);
+            // display.innerHTML='';
+            
+            display.innerText=`${(operate(operatorSign,firstNumber,secondNumber))}`;
+            operatorSign='';
+        }
+   });
+
+});
+
+
+const equal=document.getElementById('equal');
+equal.addEventListener('click', event => {
+    secondNumber=Number(display.innerHTML);
+    display.innerText=`${(operate(operatorSign,firstNumber,secondNumber))}`;
+});
+
+const clear=document.getElementById('clear');
+clear.addEventListener('click', event => {
+    operatorSign='';
+    display.innerHTML='';
+});
